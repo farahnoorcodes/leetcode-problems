b8a36cfc86c25bd80a2e22285ad39592d27f334c
Farah Noor <293658554+farahnoorcodes@users.noreply.github.com>
2026-07-18 23:20:23 +0500

    Time: 25 ms (91.44%), Space: 12.4 MB (59.33%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.py
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         sum=0
         for i in range(1,n+1):
-            if(i%3==0 or i%5==0 or i%7==0):
+            if i%3==0 or i%5==0 or i%7==0:
                 sum+=i
         return sum
         
